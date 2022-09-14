cfc65a75432965fe47a23fb86de6e348d0f1dec0
Daniel <adeboyed93@gmail.com>
2022-09-15 02:23:03 +0300

    new

--- Components/Major/Footer.tsx
+++ Components/Major/Footer.tsx
@@ -1,7 +1,6 @@
 import { Box, ListItem, UnorderedList } from "@chakra-ui/react"
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa"
 import {BsYoutube} from 'react-icons/bs'
-import {SiHashnode} from 'react-icons/Si'
 import Link from 'next/link'
 
 export const Footer =()=> {
@@ -21,7 +20,6 @@ export const Footer =()=> {
                         <Link href='#'><FaLinkedin/></Link>
                         <Link href='#'><FaTwitter/></Link>
                         <Link href='#'><BsYoutube/></Link>
-                        <Link href='#'><SiHashnode/></Link>
                     </Box>
                 </Box>
             </Box>
